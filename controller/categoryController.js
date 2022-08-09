492cf435bf5193e7766ea854633cf94da22eaf3f
Lokman Hossain Razu <89483463+LokmanRazu@users.noreply.github.com>
2022-08-09 21:06:09 +0600

    update category

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,4 +1,23 @@
 const Category = require('../model/category') ;
+const { findById } = require('../model/user');
+
+exports.getSinglecategory = async (req,res,next)=>{
+    try{
+        const category = await Category.findById({_id:req.params.id})
+        
+        res.status(200).json({
+            status:'SUCCESS',
+            result:category.length,
+            data:{
+                category
+            }
+        })
+
+    }catch(e){
+        console.log(`I am from getSingleCategory and Error is : ${e}`)
+        next(e)
+    }
+}
 
 exports.getCategory = async (req,res,next)=>{
     try{
@@ -38,4 +57,22 @@ exports.postCategory = async (req,res,next)=>{
         console.log(`I am from postCategory and Error is : ${e}`)
         next(e)
     }
-}
+};
+
+exports.updateCategory = async (req,res,next)=>{
+try{
+    const updatedCategory = await Category.findOneAndUpdate(req.params.id,req.body,{new:true})
+
+    res.status(200).json({
+        status:'SUCCESS',
+        data:{
+        updatedCategory
+        }
+    });
+
+
+}catch(e){
+    console.log(`I am from UpdateCategory and Error is : ${e}`)
+    next(e)
+  }  
+};
--- router/categoryRoutes.js
+++ router/categoryRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const { getCategory,postCategory } = require('../controller/categoryController')
+const { getCategory,getSinglecategory,postCategory,updateCategory } = require('../controller/categoryController')
 
 router.get('/',getCategory)
+router.get('/:id',getSinglecategory)
 router.post('/',postCategory)
+router.patch('/:id',updateCategory)
 
 module.exports = router
