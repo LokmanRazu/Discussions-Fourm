8b77af9f776a9530f81a1a8004ab9dff4c674462
Lokman Hossain Razu <89483463+LokmanRazu@users.noreply.github.com>
2022-09-06 00:56:21 +0600

    Err

--- index.js
+++ index.js
@@ -24,13 +24,24 @@ app.get('/',(req,res)=>{
     res.send('hello')
 })
 
-// Its Should be the Last Middleware
 app.all('*',(req,res,next)=>{
     res.status(404).json({
         status:'fail',
         message:`Can't find ${req.originalUrl} on this server`
     })
 });
+
+// ERROR HANDLING Middleware
+app.use((err,req,res,next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message
+    })
+});
+
 mongoose.connect(`mongodb+srv://lokman:pass12345@cluster0.oroe6kh.mongodb.net/?retryWrites=true&w=majority`,
 { useNewUrlParser: true }
 )
